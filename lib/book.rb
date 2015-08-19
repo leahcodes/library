06b90e41d4ed2927aa0c4f8e2ba8d5209aed3ee1
Julianna Green & Leah Klamkin <student@epicodus.com>
2015-08-19 14:44:30 -0700

    add Book class methods and specs.

---
+++ lib/book.rb
@@ -0,0 +1,51 @@
+class Book
+
+  attr_reader(:id, :title, :subject)
+
+  define_method(:initialize) do |attributes|
+    @id = attributes.fetch(:id)
+    @title = attributes.fetch(:title)
+    @subject = attributes.fetch(:subject)
+  end
+
+  define_method(:==) do |another_book|
+    self.title().==(another_book.title()).&(self.subject().==(another_book.subject())).&(self.id().==(another_book.id()))
+  end
+
+  define_singleton_method(:all) do
+    returned_books = DB.exec("SELECT * FROM books;")
+    books = []
+    returned_books.each() do |book|
+      title = book.fetch("title")
+      subject = book.fetch("subject")
+      id = book.fetch("id").to_i()
+      books.push(Book.new({:id => id, :title => title, :subject => subject}))
+    end
+    books
+  end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO books (title, subject) VALUES ('#{@title}', '#{@subject}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  define_singleton_method(:find) do |id|
+    found_book = nil
+    Book.all().each() do |book|
+      if book.id().==(id)
+        found_book = book
+      end
+    end
+    found_book
+  end
+
+  define_method(:update) do |attributes|
+    @title = attributes.fetch(:title)
+    @id = self.id()
+    DB.exec("UPDATE books SET title = '#{@title}' WHERE id = #{@id};")
+  end
+
+  define_method(:delete) do
+    DB.exec("DELETE FROM books WHERE id = #{self.id()};")
+  end
+end
---
+++ spec/book_spec.rb
@@ -0,0 +1,81 @@
+require('spec_helper')
+
+describe(Book) do
+
+  describe('.all') do
+    it('makes an empty array to store books in') do
+      expect(Book.all()).to(eq([]))
+    end
+  end
+
+  describe('#==') do
+    it("is equal if they have the same name") do
+      book1=Book.new({:id => nil, :title => "Harry Potter and the Goblet of Fire", :subject => "Fantasy"})
+      book2=Book.new({:id => nil, :title => "Harry Potter and the Goblet of Fire", :subject => "Fantasy"})
+      expect(book1).to(eq(book2))
+    end
+  end
+
+  describe('#save') do
+    it('saves a book to books array') do
+      book = Book.new({:id => nil, :title => "Harry Potter and the Goblet of Fire", :subject => "Fantasy"})
+      book.save()
+      expect(Book.all()).to(eq([book]))
+    end
+  end
+
+  describe('#id') do
+    it("sets the ID when the book is saved") do
+      book = Book.new({:id => nil, :title => "Harry Potter and the Goblet of Fire", :subject => "Fantasy"})
+      book.save()
+      expect(book.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe('#title') do
+    it('returns the title of the book') do
+      book = Book.new({:id => nil, :title => "Harry Potter and the Goblet of Fire", :subject => "Fantasy"})
+      book.save()
+      expect(book.title()).to(eq("Harry Potter and the Goblet of Fire"))
+    end
+  end
+
+  describe('#subject') do
+    it('returns the subject of the book') do
+      book = Book.new({:id => nil, :title => "Harry Potter and the Goblet of Fire", :subject => "Fantasy"})
+      book.save()
+      expect(book.subject()).to(eq("Fantasy"))
+    end
+  end
+
+  describe('.find') do
+    it('finds a book by its id') do
+      book1 = Book.new({:id => nil, :title => "Harry Potter and the Goblet of Fire", :subject => "Fantasy"})
+      book1.save()
+      book2 = Book.new({:id => nil, :title => "Harry Potter and the Half Blood Prince", :subject => "Fantasy"})
+      book2.save()
+      expect(Book.find(book1.id())).to(eq(book1))
+    end
+  end
+
+  describe('#update') do
+    it('allows a user to update information in the books table') do
+      book = Book.new({:id => nil, :title => "Harry Potter and the Goblet of Fire", :subject => "Fantasy"})
+      book.save()
+      book.update({:title => "Harry Potter and the Prisoner of Azkaban"})
+      expect(book.title()).to(eq("Harry Potter and the Prisoner of Azkaban"))
+    end
+  end
+
+  describe('#delete') do
+    it('removes a book from the table') do
+      book1 = Book.new({:id => nil, :title => "Harry Potter and the Goblet of Fire", :subject => "Fantasy"})
+      book1.save()
+      book2 = Book.new({:id => nil, :title => "Harry Potter and the Half Blood Prince", :subject => "Fantasy"})
+      book2.save()
+      book2.delete()
+      expect(Book.all()).to(eq([book1]))
+    end
+  end
+
+end
